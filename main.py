7e5c20a5619cacfea5b68a319510b217dddfb7cb
SamHaggans <sam@samhaggans.com>
2020-02-07 00:26:32 -0700

    Finish target movement

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ canvas = Canvas(master,
            height=canvas_height)
 
 canvas.pack()
-master.title("UAV Tracking Simulation")#title
-master.resizable(0, 0)#stay size always
-master.wm_attributes("-topmost", 1)#topmost window
+master.title("UAV Tracking Simulation")
+master.resizable(0, 0)
+master.wm_attributes("-topmost", 1)
 
 y = int(canvas_height / 2)
 
@@ -30,38 +30,50 @@ class Target:
         self.id = canvas.create_rectangle(self.starting[0], self.starting[1], self.starting[0]+width, self.starting[1]+width, fill = self.color)
         self.pos = [self.starting[0]+width/2, self.starting[1]+width/2]
         self.signal = Signal(self, 100)
+        self.heading = 0
     def move(self, x, y):
         self.canvas.move(self.id, x, y)
         self.pos = [self.canvas.coords(self.id)[0]+self.width/2, self.canvas.coords(self.id)[1]+self.width/2]
 
     def run(self):
+        oldPos = self.pos
         seed = random.random()
+        head_seed = random.uniform(-10, 10)
+        self.heading += head_seed
         done = False
         if seed < 0.15:
-            angle = random.uniform(0, 45)
-            newx, newy = math.cos(math.radians(angle)), math.sin(math.radians(angle))
+            angle = random.uniform(self.heading +0, self.heading +45)
+            newx, newy = 1*math.cos(math.radians(angle)), 1*math.sin(math.radians(angle))
             self.move(newx, newy)
         elif seed < 0.40:
-            angle = random.uniform(45, 90)
-            newx, newy = math.cos(math.radians(angle)), math.sin(math.radians(angle))
+            angle = random.uniform(self.heading +45, self.heading +90)
+            newx, newy = 1*math.cos(math.radians(angle)), 1*math.sin(math.radians(angle))
             self.move(newx, newy)
         elif seed < 0.65:
-            angle = random.uniform(90, 135)
-            newx, newy = math.cos(math.radians(angle)), math.sin(math.radians(angle))
+            angle = random.uniform(self.heading +90, self.heading +135)
+            newx, newy = 1*math.cos(math.radians(angle)), 1*math.sin(math.radians(angle))
             self.move(newx, newy)
         elif seed < 0.8:
-            angle = random.uniform(135, 180)
-            newx, newy = math.cos(math.radians(angle)), math.sin(math.radians(angle))
+            angle = random.uniform(self.heading +135, self.heading +180)
+            newx, newy = 1*math.cos(math.radians(angle)), 1*math.sin(math.radians(angle))
             self.move(newx, newy)
         elif seed < 0.9:
-            angle = random.uniform(180, 225)
-            newx, newy = math.cos(math.radians(angle)), math.sin(math.radians(angle))
+            angle = random.uniform(self.heading +180, self.heading +225)
+            newx, newy = 1*math.cos(math.radians(angle)), 1*math.sin(math.radians(angle))
             self.move(newx, newy)
         else:
-            angle = random.uniform(315, 360)
-            newx, newy = math.cos(math.radians(angle)), math.sin(math.radians(angle))
+            angle = random.uniform(self.heading +315, self.heading +360)
+            newx, newy = 1*math.cos(math.radians(angle)), 1*math.sin(math.radians(angle))
             self.move(newx, newy)
-
+        if self.pos[0] > 1450:
+            self.move(1450-self.pos[0], 0)
+        if self.pos[0] < 50:
+            self.move(50-self.pos[0], 0)
+        if self.pos[1] > 650:
+            self.move(0, 650-self.pos[1])
+        if self.pos[1] < 50:
+            self.move(0, 50-self.pos[1])
+        canvas.create_line(oldPos[0], oldPos[1], self.pos[0], self.pos[1], fill="orange red")
 class UAV:
     def __init__(self, canvas, color, starting, width):
         self.canvas = canvas
@@ -102,8 +114,12 @@ def animation(width, height):
     master.update_idletasks()#needed tkinter things
     master.update()
     while True:
-        #target.run()
+        target.run()
+        print(drone.getSignalStrength(target.signal))
+        time.sleep(0.03)
         master.update_idletasks()#needed tkinter things
         master.update()
-mainloop()
+
 animation(canvas_width, canvas_height)
+mainloop()
+
